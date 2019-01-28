eb439d41efe1f12be712496c4e92df45dd237001
zl ding <dzl84394@163.com>
2019-01-28 10:35:34 +0800

    code commid

--- ums/dms-ums-server/src/main/java/com/dms/EurekaServerApplication.java
+++ ums/dms-ums-server/src/main/java/com/dms/EurekaServerApplication.java
@@ -1,13 +1,18 @@
 package com.dms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class EurekaServerApplication {
 	public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
+	
+	
+	
 }
